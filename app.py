a3ff926e10242900da73583b08e73553dd9468de
Joshua Peters <Joshua.Peters@colorado.edu>
2025-04-01 22:01:07 -0600

    Added db_create route

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+#
+# This script was written by: Joshua Peters
+# Date: 4/1/25
+# Modified: 4/1/2025
+# Purpose: 
+# Usage:
+
 from flask import Flask
 import psycopg2
 app = Flask(__name__)
@@ -11,3 +18,23 @@ def db_test():
     conn = psycopg2.connect("postgresql://lab_10_database_neu6_user:R4HMkxxlwa3rjNVNE29EUhRn505UKS17@dpg-cvmb2mnfte5s73akthv0-a/lab_10_database_neu6")
     conn.close()
     return "Database Connection Successful"
+
+@app.route('/db_create')
+def db_create():
+    conn = psycopg2.connect("postgresql://lab_10_database_neu6_user:R4HMkxxlwa3rjNVNE29EUhRn505UKS17@dpg-cvmb2mnfte5s73akthv0-a/lab_10_database_neu6")
+    cur = conn.cursor()
+
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+    ''')
+
+    conn.commit()
+    conn.close()
+
+    return "Basketball Table Successfully Created"

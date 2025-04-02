ba5c66588a554be179abdf3844e486a57d63a915
Joshua Peters <Joshua.Peters@colorado.edu>
2025-04-01 21:53:10 -0600

    Fixed db_test

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ def hello_world():
     return 'Hello World from Joshua P. in 3308'
 
 @app.route('db_test')
+def db_test():
     conn = psycopg2.connect("postgresql://lab_10_database_neu6_user:R4HMkxxlwa3rjNVNE29EUhRn505UKS17@dpg-cvmb2mnfte5s73akthv0-a/lab_10_database_neu6")
     conn.close()
     return "Database Connection Successful"

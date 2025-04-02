6d6ac84ee89e66a7f6dc9ffe8c49779a1db826a0
Joshua Peters <Joshua.Peters@colorado.edu>
2025-04-01 21:55:22 -0600

    Really fixed db_test

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World from Joshua P. in 3308'
 
-@app.route('db_test')
+@app.route('/db_test')
 def db_test():
     conn = psycopg2.connect("postgresql://lab_10_database_neu6_user:R4HMkxxlwa3rjNVNE29EUhRn505UKS17@dpg-cvmb2mnfte5s73akthv0-a/lab_10_database_neu6")
     conn.close()

3cdee02f46717ac81f609c41f1943ba2ec3b63c5
Joshua Peters <Joshua.Peters@colorado.edu>
2025-04-01 21:46:15 -0600

    Added db_test route

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+import psycopg2
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World from Joshua Peters in 3308'
+    return 'Hello World from Joshua P. in 3308'
+
+@app.route('db_test')
+    conn = psycopg2.connect("postgresql://lab_10_database_neu6_user:R4HMkxxlwa3rjNVNE29EUhRn505UKS17@dpg-cvmb2mnfte5s73akthv0-a/lab_10_database_neu6")
+    conn.close()
+    return "Database Connection Successful"

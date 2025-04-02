2298013d94aba1c4e0eb87a87aed98b407fa7544
Joshua Peters <Joshua.Peters@colorado.edu>
2025-04-01 22:10:48 -0600

    Added db_select route

--- app.py
+++ app.py
@@ -57,3 +57,29 @@ def db_insert():
     conn.close()
 
     return "Basketball Table Successfully Populated"
+
+@app.route('/db_select')
+def db_select():
+    conn = psycopg2.connect("postgresql://lab_10_database_neu6_user:R4HMkxxlwa3rjNVNE29EUhRn505UKS17@dpg-cvmb2mnfte5s73akthv0-a/lab_10_database_neu6")
+    cur = conn.cursor()
+
+    cur.execute(''''
+        SELECT * FROM Basketball;
+        ''')
+    
+    records = cur.fetchall()
+    conn.close()
+
+    response_string =""
+    response_string += "<table>"
+
+    for player in records:
+        response_string += "<tr>"
+        
+        for info in player:
+            response_string += "<td>{}</td>".format(info)
+        
+        response_string += "</tr>"
+    response_string += "</table>"
+
+    return response_string
